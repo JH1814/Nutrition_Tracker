964ffe88a5e6a84b8f2e195ba7509276df2e2f91
Paulo-Pereira-FHNW <paulo.pereira@students.fhnw.ch>
2025-11-12 08:20:05 +0000

    new main

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import data
 import ui
+import datetime
 
 running = True
 
@@ -8,18 +9,31 @@ def main():
     ui.showMainMenu()
     choice = input("Enter your choice: ")
     if choice == "1":
+        name = print("Add Name of Nutrition Entry")
+        carbs = print("Add Carbs in grams")
+        protein = print("Add Protein in grams")
+        fats = print("Add Fats in grams")
+        date = datetime.datetime.now()
 
-        # ui.getNutritionInput()  # This function would get user input for nutrition data
+        try :
+            data.writeNutritionData({"Name": name, "Carbs": carbs, "Protein": protein, "Fats": fats, "Date": date})
+            ui.addNutritionSuccessfull()
+        except FileNotFoundError as e:
+            ui.addNutritionFailed(e)
+
+    if choice == "2":
+        recipie = input("Enter the name of the recipe to use from the Nutrition Entry List: ")
+        entries = data.getEntryByName(recipie)
+       
+        try :
+            data.writeNutritionData({"Name": entries.name, "Carbs": entries.carbs, "Protein": entries.protein, "Fats": fats, "Date": entries.date})
+            ui.addNutritionSuccessfull()
+        except FileNotFoundError as e:
+            ui.addNutritionFailed(e)
 
 # if choice =="5":
     #running = False
     #exit()
-
-        try :
-            data.writeNutritionData({date: "2024-06-01", protein: 50, carbs: 200, fats: 70})
-            ui.addNutritionSuccessfull()
-        except FileNotFoundError as e:
-            ui.addNutritionFailed(e)
 
 
 
